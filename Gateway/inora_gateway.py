acb986756584c1aa7dcf3f3a79412f7e7c9d5c8b
fclerg <fclerget@yahoo.fr>
2017-07-23 14:35:43 +0100

    initial commit

---
+++ Gateway/inora_gateway.py
@@ -0,0 +1,69 @@
+"""
+   Inora Gateway execution main script
+"""
+
+# pylint: disable=line-too-long
+import ConfigParser
+import json
+import os.path
+from inspect import getsourcefile
+import lib.devicesextractor as devicesextractor
+from lib.inoralogger import InoraLogger
+from lib.eventsender import EventSender
+from lib.devicefilter import DeviceFilter
+
+
+
+##############################################################################
+#                             LOGGER INIT                                    #
+##############################################################################
+
+LOGGING = InoraLogger.get_logger()
+
+
+##############################################################################
+#                             CONF HANDLING                                  #
+##############################################################################
+
+def get_conf_value(sectionval, optionval, isfile=False):
+    """parse the config file and return the value of the given variable"""
+    wconfig = ConfigParser.SafeConfigParser()
+    # Tweak to retrieve the path
+    conf_path = wconfig.read(os.path.dirname(getsourcefile(lambda: None)) + '/inora.conf')
+    if not wconfig.has_option(sectionval, optionval):
+        LOGGING.error('in ' + str(conf_path) + ': section:' + sectionval + ' or option:' + optionval + ' missing')
+        exit(1)
+    if isfile:
+        if not os.path.isfile(wconfig.get(sectionval, optionval)):  # Check if file exists
+            LOGGING.error(" " + wconfig.get(sectionval, optionval) + " NOT FOUND!")
+            exit(1)
+    LOGGING.debug('in ' + str(conf_path) + ': section:' + sectionval + ' and option:' + optionval + ' Validated')
+    return str(wconfig.get(sectionval, optionval))
+
+
+##############################################################################
+#                             Let's get started                              #
+##############################################################################
+
+
+
+def main():
+    """Launch the Inora Gateway"""
+    router_ip_address = get_conf_value('general_conf', 'router_ip')
+    server_ip_address = get_conf_value('general_conf', 'server_ip')
+    server_port = get_conf_value('general_conf', 'server_port')
+    poll_period = get_conf_value('general_conf', 'poll_period')
+    get_conf_value('certs_conf', 'enable_truth')
+    get_conf_value('certs_conf', 'server_cert_path', isfile=True)
+    get_conf_value('certs_conf', 'private_key_path', isfile=True)
+    get_conf_value('handler_file', 'args')
+
+    LOGGING.info('Starting...')
+    dictdevices = devicesextractor.SuperHubConnectedDevicesExtractor(router_ip_address, poll_period)
+    wfilter = DeviceFilter('./devices.filters')
+    while True:
+        sender = EventSender(server_ip_address, server_port)
+        sender.send_https_message(json.dumps(wfilter.filter(json.dumps(dictdevices.get_devices_dict(), indent=2)), indent=2))
+
+if __name__ == '__main__':
+    main()
---
+++ Server/inora_server.py
@@ -0,0 +1,73 @@
+'''Inora Server execution main script'''
+# pylint: disable=line-too-long
+
+import ssl
+import ConfigParser
+import os.path
+from inspect import getsourcefile
+from BaseHTTPServer import HTTPServer
+from lib.inorahttprequesthandler import inoraHTTPRequestHandler
+from lib.inoralogger import InoraLogger
+from lib.rsspopulater import RSSPopulater
+
+class MyHTTPServer(HTTPServer):
+    """this HTTPServer sub-class is necessary to allow passing custom request
+       handler into the RequestHandlerClass"""
+    def __init__(self, server_address, RequestHandlerClass, rsspopulater):
+        HTTPServer.__init__(self, server_address, RequestHandlerClass)
+        self.rss_populater = rsspopulater
+
+
+##############################################################################
+#                             INIT LOGGER                                    #
+##############################################################################
+
+LOGGING = InoraLogger.get_logger()
+
+
+##############################################################################
+#                             CONF HANDLING                                  #
+##############################################################################
+
+def get_conf_value(sectionval, optionval, isfile=False):
+    """parse the config file and return the value of the given variable"""
+    wconfig = ConfigParser.SafeConfigParser()
+    # Tweak to retrieve the path
+    conf_path = wconfig.read(os.path.dirname(getsourcefile(lambda: None)) + '/inora.conf')
+    if not wconfig.has_option(sectionval, optionval):
+        LOGGING.error('in ' + str(conf_path) + ': section:' + sectionval + ' or option:' + optionval + ' missing')
+        exit(1)
+    if isfile:
+        if not os.path.isfile(wconfig.get(sectionval, optionval)):  # Check if file exists
+            LOGGING.error(" " + wconfig.get(sectionval, optionval) + " NOT FOUND!")
+            exit(1)
+    LOGGING.debug('in ' + str(conf_path) + ': section:' + sectionval + ' and option:' + optionval + ' Validated')
+    return str(wconfig.get(sectionval, optionval))
+
+
+##############################################################################
+#                             Let's Start!                                   #
+##############################################################################
+
+def main():
+    """Launch the Inora Server"""
+    server_ip_address = get_conf_value('general_conf', 'ip_address')
+    server_port = get_conf_value('general_conf', 'port')
+    web_root = get_conf_value('general_conf', 'web_root')
+    rss_key = get_conf_value('general_conf', 'rss_key')
+    get_conf_value('certs_conf', 'enable_truth')
+    cert_path = get_conf_value('certs_conf', 'server_cert_path', isfile=True)
+    key_path = get_conf_value('certs_conf', 'private_key_path', isfile=True)
+    LOGGING.info("Starting Server on port:" + server_port)
+    rsspopulater = RSSPopulater(web_root + '/' + rss_key)
+
+    #Create a web server and define the handler to manage the incoming request
+    #WARNING : We don't use server_ip_address as first parameter but leave it blank or won't handle requests
+    server = MyHTTPServer(('', int(server_port)), inoraHTTPRequestHandler, rsspopulater)
+    server.socket = ssl.wrap_socket(server.socket, keyfile=key_path, certfile=cert_path, server_side=True)
+    LOGGING.info('Started httpserver on port ' + server_port)
+    #Wait forever for incoming http requests
+    server.serve_forever()
+
+if __name__ == '__main__':
+    main()
