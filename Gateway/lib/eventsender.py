6db9e26094c72ad05b5f9ae941e2795650f012e8
Francois <fclerget@yahoo.fr>
2017-08-29 12:00:45 +0100

    adding timeout for POST messages from Gateway to Server

--- Gateway/lib/eventsender.py
+++ Gateway/lib/eventsender.py
@@ -1,10 +1,11 @@
 """Lib for handling message sending to the Inora server"""
 # pylint: disable=line-too-long
+import time
 import requests
 import lib.inoralogger
 LOGGING = lib.inoralogger.InoraLogger.get_logger()
 
-class EventSender:
+class EventSender(object):
     """Class to send messages over IP"""
 
     def __init__(self, ip_address, port):
@@ -15,8 +16,14 @@ class EventSender:
         """sending a message over https"""
         url = 'https://' + self.__ip_address + ':' + str(self.__port)
         headers = {'Content-Length': str(len(message))}
-        try:
-            LOGGING.debug(" Sending poll result over Https. dst_IP:" + self.__ip_address + " port:" + str(self.__port))
-            requests.post(url, data=message, headers=headers, verify=False)
-        except requests.exceptions.ConnectionError as e:
-            LOGGING.error(e)
+        # tweak in case of Connection exception
+        while True:
+            try:
+                LOGGING.debug(" Sending poll result over Https. dst_IP:%s port:%s", self.__ip_address, self.__port)
+                requests.post(url, data=message, headers=headers, verify=False, timeout=30)
+            except (requests.exceptions.Timeout, requests.exceptions.ConnectionError) as exception:
+                LOGGING.error(exception)
+                LOGGING.info("Retrying to connect...")
+                time.sleep(3)
+                continue
+            break
